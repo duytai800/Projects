14243b73a490ee39076c328d62a2e21445e58fc8
duytai800 <duytai800@gmail.com>
2021-11-29 15:18:30 +0700

    Description

---
+++ Competitions/Tempo Run/StrokePrediction.py
@@ -0,0 +1,5 @@
+# This is the project that my teammates and I did when join in the AI Tempo Run Competition. 
+# They provided us with a train set, including patient information and labels. 
+# Our task is to estimate whether a patient is likely to get stroke.
+# We entered the competition as freshmen and ranked fourth overall.
+# Google Colab: https://colab.research.google.com/drive/1_An9b0jadT3ktH5aVVlwbHjtxS_2RIuy?usp=sharing
